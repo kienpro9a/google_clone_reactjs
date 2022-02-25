2737934a8ec2b5f6e5f5365ccc2bb9d6f8f785d5
kienpro9a <70696780+kienpro9a@users.noreply.github.com>
2022-02-25 10:16:14 +0700

    small update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';//
 import HomePage from './components/homePage';
 import SearchPage from './components/searchPage';
@@ -6,21 +6,20 @@ import { searchData } from './api/googleSearch';
 
 const App = () => {
   const navigate = useNavigate();
-  const [searchTerm, setSearchTerm] = useState(JSON.parse(localStorage.getItem('SearchTerm')));
-  const [googleData, setGoogleData] = useState(JSON.parse(localStorage.getItem('GoogleData')));
+  const [searchTerm, setSearchTerm] = useState(JSON.parse(localStorage.getItem('SearchTerm')) || "");
+  const [googleData, setGoogleData] = useState(JSON.parse(localStorage.getItem('GoogleData')) || []);
   const setSearch = async (term) => {
     setSearchTerm(term);
     const data = await searchData(term);
     setGoogleData(data);
     navigate('/search');
   }
-  
+
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<HomePage setSearch={setSearch} />} />
         <Route exact path="/search" element={searchTerm !== "" ? <SearchPage setSearch={setSearch} searchTerm={searchTerm} googleData={googleData} /> : <Navigate to="/" />} />
-        {/* <Route exact path="/search" element={<SearchPage setSearch={setSearch} searchTerm={searchTerm} googleData={googleData} />} /> */}
       </Routes>
     </div>
   );
